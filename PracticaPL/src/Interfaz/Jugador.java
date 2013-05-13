1660cbce341572125fbf17d42af24cad6ad4a856
Antonio Cubero <acuberof@gmail.com>
2013-05-13 12:13:53 +0200

    Interfaz v1

---
+++ PracticaPL/src/Interfaz/Jugador.java
@@ -0,0 +1,38 @@
+package Interfaz;
+
+public class Jugador {
+	int vidas;
+	int x, y;
+
+	Jugador(){
+		x = 0;
+		y = 0;
+		vidas = 3;
+	}
+
+	public int getVidas() {
+		return vidas;
+	}
+
+	public void setVidas(int vidas) {
+		this.vidas = vidas;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
+}
